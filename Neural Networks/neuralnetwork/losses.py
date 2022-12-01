b3ff7cc76d9c202a749eaa8e93d5e1d7d300c9f6
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-12-01 23:24:29 +0530

    Create losses.py

---
+++ Neural Networks/neuralnetwork/losses.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(self, output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+    
+class CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+
+        elif len(y_true.shape) == 2: ## Incase user passes one-hot encoded vector.
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
